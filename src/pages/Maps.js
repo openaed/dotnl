f55905ee337587b0114135839d521e1c391ea99a
jxpsert <j.platenburg@student.han.nl>
2023-05-03 12:57:39 +0200

    use description instead of locations

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -26,7 +26,7 @@ export default async function GetMaps() {
 
       return {
         name: placesResult.data.name,
-        locations: placesResult.data.description,
+        description: placesResult.data.description,
         url: `https://${map}.openaed.nl/`,
       };
     })
