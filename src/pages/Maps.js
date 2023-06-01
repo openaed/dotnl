ee1ee26230bc225cdd8f090dbac1cce7c7469a86
jxpsert <j.platenburg@student.han.nl>
2023-06-01 12:56:06 +0200

    foto in hero toegevoegd

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,20 +1,3 @@
-// export default async function GetMaps() {
-//   const maps = [];
-//   const mapsResult = await (await fetch("https://api.openaed.nl/maps")).json();
-
-//   mapsResult.data.forEach(async (map) => {
-//     const placesResult = await (
-//       await fetch(`https://api.openaed.nl/${map}/places`)
-//     ).json();
-//     maps.push({
-//       name: placesResult.data.name,
-//       locations: placesResult.data.places,
-//       url: `https://${map}.openaed.nl/`,
-//     });
-//   });
-//   return maps;
-// }
-
 export default async function GetMaps() {
   const mapsResult = await (await fetch("https://api.openaed.nl/maps")).json();
 
--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,6 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
+import ptdklogo from "../ptdk.svg";
+
 function Navigation() {
   return (
     <Navbar bg="success" expand="lg" className="navbar-fixed-top">
@@ -8,6 +10,10 @@ function Navigation() {
         <Navbar.Brand href="/" className="text-white fw-bold">
           OpenAED.nl
         </Navbar.Brand>
+        <a href="https://ptdk.nl" target="_blank" class="text-white td-none" rel="noreferrer">
+          mogelijk gemaakt door&nbsp;
+          <img src={ptdklogo} alt="potato donkey logo" class="ptdklogo" />
+        </a>
       </Container>
     </Navbar>
   );
